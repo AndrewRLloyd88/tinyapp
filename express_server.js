377310e123f8541d52c01b7fa940b0f3491e8269
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-02 12:24:16 -0700

    change randomGeneration to use UUID and install morgan package to track each transaction

--- express_server.js
+++ express_server.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const uuid = require('uuid');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
+// morgan middleware allows to log the request in the terminal
+app.use(morgan('short'));
 
+//keeps track of URLS belonging to the specific user logged in, populated by checking userID against userDB shortURL ids.
 let userURLS = {};
 
 //returns 6 random characters from characters and associates the new tinyURL to a longURL
 const generateRandomString = () => {
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let tinyURL = "";
-  for (let i = 0; i < 6; i++) {
+  // const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const randomID = uuid.v4().substr(0, 6);
+  // for (let i = 0; i < 6; i++) {
     //use round to generate a number that can round up to between 0 and characters.length
-    const randomNum = Math.floor(Math.random() * characters.length);
-    tinyURL += characters[randomNum];
-  }
-  return tinyURL;
+    // const randomNum = Math.floor(Math.random() * characters.length);
+    // tinyURL = 
+  // }
+  return randomID;
 };
 
 //database is not yet persistent when server restarts
@@ -90,7 +95,7 @@ const passwordCheck = (password) => {
     }
   }
   return false;
-}
+};
 
 //checks if fields are empty -- refactor this check
 const checkFieldsPopulated = (email, password) => {
@@ -99,7 +104,7 @@ const checkFieldsPopulated = (email, password) => {
     return false;
   }
   return true;
-}
+};
 
 //checks what value req.cookies.user_id is. If undefined user !== logged in
 const isLoggedIn = (req) => {
@@ -109,33 +114,35 @@ const isLoggedIn = (req) => {
   } else {
     return true;
   }
-}
+};
 
 
 //returns the URLs where the userID is equal to the field of the currently logged in user
 const urlsForUser = (id) => {
   //remember to clear userURLS
-  userURLS = {}
+  userURLS = {};
   for (const url in urlDatabase) {
     if (id === urlDatabase[url].userID) {
-      userURLS[url] = urlDatabase[url].longURL
+      userURLS[url] = urlDatabase[url].longURL;
     }
   }
-  console.log(userURLS)
+  console.log(userURLS);
   return userURLS;
-}
+};
 
+//checks the incoming request from a user to make sure they own that url.
+//Stops users editing other users shortURLS if they dont own it.
 const checkUserOwnsURL = (id, request) => {
   // console.log("request: " + request)
-  for (urls in urlDatabase) {
+  for (const urls in urlDatabase) {
     if (urlDatabase[request].userID !== id) {
       // console.log(request.user_id + " " + "in checkUserOwnsUrl")
-      return false
+      return false;
     } else {
       return true;
     }
   }
-}
+};
 
 
 // Edge cases
@@ -160,20 +167,18 @@ const checkUserOwnsURL = (id, request) => {
 app.get("/login", (req, res) => {
   let templateVars = { user_id: req.cookies["user_id"], userEmail: checkUserId(req.cookies['user_id']) };
   res.render('login', templateVars);
-})
+});
 
 //handles login requests
 app.post("/login", (req, res) => {
   //are our login fields populated?
   if (!checkFieldsPopulated(req.body.email, req.body.password)) {
     res.sendStatus(400);
-  }
-  //does email match an email on our db?
-  else if (!emailLookup(req.body.email)) {
+    //does email match an email on our db?
+  } else if (!emailLookup(req.body.email)) {
     res.sendStatus(403);
-  }
-  //does the password match?
-  else if (!passwordCheck(req.body.password)) {
+    //does the password match?
+  } else if (!passwordCheck(req.body.password)) {
     res.sendStatus(403);
   } else {
     //need to check all users to see if an email matches and if so set cookie to user_id : userDatabase[randomID]
@@ -181,7 +186,7 @@ app.post("/login", (req, res) => {
     for (const users in userDatabase) {
       if (req.body.email === userDatabase[users].email) {
         res.cookie("user_id", userDatabase[users].id);
-        userURLS = urlsForUser(req.cookies.user_id)
+        userURLS = urlsForUser(req.cookies.user_id);
       }
     }
 
@@ -193,7 +198,7 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   //set the user_id cookie to an empty string on logout
   res.clearCookie("user_id");
-  userURLS = {}
+  userURLS = {};
   res.redirect("/login");
 });
 
@@ -210,9 +215,8 @@ app.post("/register", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
     //send a 400 error - Bad Request
     res.sendStatus(400);
-  }
-  //check if someone tries to register an already registered email address
-  else if (emailLookup(req.body.email)) {
+    //check if someone tries to register an already registered email address
+  } else if (emailLookup(req.body.email)) {
     res.sendStatus(400);
   } else {
 
@@ -265,7 +269,7 @@ app.get("/urls.json", (req, res) => {
 //displays the current url database
 app.get("/urls", (req, res) => {
 
-  urlsForUser(req.cookies.user_id)
+  urlsForUser(req.cookies.user_id);
 
   let templateVars = {
     urls: userURLS,
@@ -297,7 +301,7 @@ app.get("/urls/new", (req, res) => {
 
 //handles a redirect from the u/shortURL to the full longURL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[`${req.params.shortURL}`]["longURL"]
+  const longURL = urlDatabase[`${req.params.shortURL}`]["longURL"];
   res.redirect(longURL);
 });
 
@@ -346,7 +350,7 @@ app.post("/urls/:shortURL/update", (req, res) => {
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   let shortURL = generateRandomString(); //Log the randomly generated tinyURL to the console
-  console.log(req.body.longURL)
+  console.log(req.body.longURL);
   urlDatabase[shortURL] = { longURL: req.body.longURL, userID: req.cookies.user_id }; //send the new shortURL and longURL to urlDatabase
   console.log(urlDatabase); //log the urlDatabase to check the new values get added ok.
   res.redirect(`/urls/${shortURL}`); // redirection to /urls/:shortURL, where shortURL is the random string we generated.
