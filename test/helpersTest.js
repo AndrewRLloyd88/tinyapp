7cc0f060c2bf20f84080bda2ca32e1c1f2722714
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-09-07 08:00:40 -0700

    performed eslint --fix to fix all issues with test/helpersTest.js

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,29 +4,29 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
-    assert.equal(user, expectedOutput)
+    assert.equal(user, expectedOutput);
   });
 });
 
 describe('getUserByEmail', function() {
   it('should return false if a user does not exist', function() {
-    const user = getUserByEmail("user@exmple.com", testUsers)
+    const user = getUserByEmail("user@exmple.com", testUsers);
     const expectedOutput = false;
-    assert.equal(user, expectedOutput)
+    assert.equal(user, expectedOutput);
   });
 });
